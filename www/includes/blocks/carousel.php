8314f5246f5682132ca0cc61c1770f23534e8a6e
Elijah <graff13@rambler.ru>
2014-10-06 11:09:17 +0300

    syntax error debug

--- www/includes/blocks/carousel.php
+++ www/includes/blocks/carousel.php
@@ -8,35 +8,40 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/scripts/config_gallery.php";
     <div class="wrapper-inner-container">
         <div class="gallery">
             <div class="gallery1">
-                <?php	
-			    foreach($img as $key => $value){
-			    echo '<a href="'. $value .'"><img src="' . $_SERVER["DOCUMENT_ROOT"] . $key . '"/>';	
-                ?>
-            </div>
-        
-        
-            <div class="gallery-scroll-box">
                 <?php
 
-                $i = 0;
-                while(($item = array_shift($news)) && ($i <= 3)){
-                    if ($item["type"] != NEWS_TYPE_NEWS){
-                        continue;
-                    }
-
-                    $i++;
-                    $img_url = PATH_NEWS_IMAGES . $item["image"] . ".jpg";
+			    foreach($img as $key => $value){
+                    echo '<a href="'. $value .'"><img src="' . $_SERVER["DOCUMENT_ROOT"] . $key . '"/>';
 
                     ?>
-                    <div class="news-item">
-
-                        <a href="/news/<?php echo $item["label"]?>"><img src="<?php echo $img_url; ?>" alt="<?php echo $item["short"]; ?>" /></a>
-                        <div class="text-block">
-                            <a href="/news/<?php echo $item["label"]?>"><?php echo $item["short"];?></a>
-                        </div>
                     </div>
-                <?php
 
+                    <div class="gallery-scroll-box">
+                    <?php
+
+                    $i = 0;
+                    while(($item = array_shift($news)) && ($i <= 3)){
+                        if ($item["type"] != NEWS_TYPE_NEWS){
+                            continue;
+                        }
+
+                        $i++;
+                        $img_url = PATH_NEWS_IMAGES . $item["image"] . ".jpg";
+
+                        ?>
+                        <div class="news-item">
+                            <a href="/news/<?php echo $item["label"]?>">
+                                <img src="<?php echo $img_url; ?>" alt="<?php echo $item["short"]; ?>" />
+                            </a>
+                            <div class="text-block">
+                                <a href="/news/<?php echo $item["label"]?>">
+                                    <?php echo $item["short"];?>
+                                </a>
+                            </div>
+                        </div>
+                    <?php
+
+                    }
                 }
 
                 /*
