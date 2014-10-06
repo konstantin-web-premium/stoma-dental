d38e53377e3ed0f7cad1aa38a81d509564cc58f8
mersua <bars909@mail.ru>
2014-10-06 10:08:32 +0300

    Создаем конфиг карусели

---
+++ www/scripts/config_gallery.php
@@ -0,0 +1,8 @@
+<?php
+//key is the path to the images, and the value - link
+
+$img = array(
+    "/images/gallery/img1.jpg" => "http://php.net/",
+    "/images/gallery/img-implay.jpg" => "http://php.net/",
+);
+?>
