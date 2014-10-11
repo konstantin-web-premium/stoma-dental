8965060afe9428aeba7c7ceb4e51d3cc88b6efa6
mersua <bars909@mail.ru>
2014-10-11 14:17:10 +0300

    Update carousel with links

--- www/scripts/jquery_gallery.js
+++ www/scripts/jquery_gallery.js
@@ -1,6 +1,12 @@
-$(function(){
-$('.gallery1 img:gt(0)').hide();
-setInterval(function()
-{$('.gallery1 :first-child').fadeOut(1000).next('img').fadeIn(1000)
-.end().appendTo('.gallery1');}, 5000);
+$(document).ready(function(){
+  slideShow();
 });
+ 
+function slideShow() {
+  var current = $('#photos .show');
+  var next = current.next().length ? current.next() : current.siblings().first();
+ 
+  current.hide().removeClass('show');
+  next.fadeIn().addClass('show');
+  setTimeout(slideShow, 5000);
+}
